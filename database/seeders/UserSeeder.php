3386ea9b02fb91819c438e838d038a33e581dc67
z-fujimori <fuji.tai.29@i.softbank.jp>
2024-07-30 08:31:50 +0000

    7/30

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\DateTime;
-
+// use Illuminate\Support\Facades\DateTime;
+use DateTime;
 
 class UserSeeder extends Seeder
 {
@@ -20,22 +20,30 @@ class UserSeeder extends Seeder
                 [
                     'name' => 'test',
                     'email' => 'test@test',
-                    'password' => Hash::make('testtest')
+                    'password' => Hash::make('testtest'),
+                    'created_at' => new DateTime(),
+                    'updated_at' => new DateTime(),
                 ],
                 [
                     'name' => 'test2',
                     'email' => 'test2@test',
-                    'password' => Hash::make('testtest')
+                    'password' => Hash::make('testtest'),
+                    'created_at' => new DateTime(),
+                    'updated_at' => new DateTime(),
                 ],
                 [
                     'name' => 'テストユーザー',
                     'email' => 'testuser@test',
-                    'password' => Hash::make('testtest')
+                    'password' => Hash::make('testtest'),
+                    'created_at' => new DateTime(),
+                    'updated_at' => new DateTime(),
                 ],
                 [
                     'name' => 'てす',
                     'email' => 'tes@test',
-                    'password' => Hash::make('testtest')
+                    'password' => Hash::make('testtest'),
+                    'created_at' => new DateTime(),
+                    'updated_at' => new DateTime(),
                 ]
             ];
         
